5fdd9b315fba561937376cd0f8b274f93925d5be
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2015-10-29 05:07:45 +0800

    Reformat to follow PEP 8

--- cross_stitch.py
+++ cross_stitch.py
@@ -13,48 +13,50 @@ PIXEL_DRAW_THRESHOLD = 230
 EAST = True
 WEST = False
 
+
 class Pixel():
-	"""docstring for Pixel"""
-	def __init__(self, x, y, rgb):
-		self.x = x
-		self.y = y
-		self.rgb = rgb
+    """docstring for Pixel"""
+
+    def __init__(self, x, y, rgb):
+        self.x = x
+        self.y = y
+        self.rgb = rgb
+
 
 def stitch(pixels, length=FRAME_SIZE):
+    turtle.left(45)
 
-	turtle.left(45)
+    half_frame = FRAME_SIZE / 2
 
-	half_frame = FRAME_SIZE / 2
+    forward_distance = sqrt(
+        (FRAME_SIZE / length) ** 2 +
+        (FRAME_SIZE / length) ** 2)
 
-	forward_distance = sqrt(
-		(FRAME_SIZE/length) ** 2 +
-		(FRAME_SIZE/length) ** 2)
+    for pixel in pixels:
+        turtle.penup()
+        turtle.goto(
+            pixel.x * FRAME_SIZE / length - half_frame,
+            pixel.y * FRAME_SIZE / length - half_frame
+        )
+        turtle.pendown()
 
-	for pixel in pixels:
-		turtle.penup()
-		turtle.goto(
-			pixel.x*FRAME_SIZE/length-half_frame,
-			pixel.y*FRAME_SIZE/length-half_frame
-		)
-		turtle.pendown()
+        turtle.pencolor(pixel.rgb)
+        turtle.forward(forward_distance)
 
-		turtle.pencolor(pixel.rgb)
-		turtle.forward(forward_distance)
+    turtle.right(90)
 
-	turtle.right(90)
+    for pixel in reversed(pixels):
+        turtle.penup()
+        turtle.goto(
+            pixel.x * FRAME_SIZE / length - half_frame,
+            (pixel.y + 1) * FRAME_SIZE / length - half_frame
+        )
+        turtle.pendown()
 
-	for pixel in reversed(pixels):
-		turtle.penup()
-		turtle.goto(
-			pixel.x*FRAME_SIZE/length-half_frame,
-			(pixel.y+1)*FRAME_SIZE/length-half_frame
-		)
-		turtle.pendown()
+        turtle.pencolor(pixel.rgb)
+        turtle.forward(forward_distance)
 
-		turtle.pencolor(pixel.rgb)
-		turtle.forward(forward_distance)
-
-	turtle.left(45)
+    turtle.left(45)
 
 
 def get_pixel_average(pixel):
@@ -62,32 +64,31 @@ def get_pixel_average(pixel):
 
 
 def get_pixel_difference(pixel1, pixel2):
-	pixel_average1 = get_pixel_average(pixel1)
-	pixel_average2 = get_pixel_average(pixel2)
+    pixel_average1 = get_pixel_average(pixel1)
+    pixel_average2 = get_pixel_average(pixel2)
 
-	return abs(pixel_average1 - pixel_average2)
+    return abs(pixel_average1 - pixel_average2)
 
 
 def get_pixels_to_draw(pixels, length, rgb):
-	direction = EAST
+    direction = EAST
 
-	for y in range(length):
-		row = range(length)
-		if direction == WEST:
-			row.reverse()
+    for y in range(length):
+        row = range(length)
+        if direction == WEST:
+            row.reverse()
 
-		found = False
-		for x in row:
-			pixel = pixels[x, y]
-			pixel_average = sum(pixel) / len(pixel)
-			if get_pixel_difference(pixel, rgb) < PIXEL_DIFFERENCE_THRESHOLD \
-                                and pixel_average < PIXEL_DRAW_THRESHOLD:
+        found = False
+        for x in row:
+            pixel = pixels[x, y]
+            pixel_average = sum(pixel) / len(pixel)
+            if get_pixel_difference(pixel, rgb) < PIXEL_DIFFERENCE_THRESHOLD \
+                    and pixel_average < PIXEL_DRAW_THRESHOLD:
+                yield Pixel(x, length - y, pixel)
+                found = True
 
-				yield Pixel(x, length-y, pixel)
-				found = True
-
-		if found:
-			direction = not direction
+        if found:
+            direction = not direction
 
 
 im = Image.open(filename)
@@ -103,19 +104,18 @@ colors_drawn = []
 length = im.size[1]
 
 for y in range(length):
-	for x in range(length):
-		pixel = pixels[x, y]
-		pixel_average = get_pixel_average(pixel)
+    for x in range(length):
+        pixel = pixels[x, y]
+        pixel_average = get_pixel_average(pixel)
 
-		if pixels[x, y] not in colors_drawn \
-                    and pixel_average < PIXEL_DRAW_THRESHOLD:
-			pixels_to_draw = list(get_pixels_to_draw(pixels, length, pixel))
+        if pixels[x, y] not in colors_drawn \
+                and pixel_average < PIXEL_DRAW_THRESHOLD:
+            pixels_to_draw = list(get_pixels_to_draw(pixels, length, pixel))
 
-			for pixel_to_draw in pixels_to_draw:
-				if pixel_to_draw.rgb not in colors_drawn:
-					colors_drawn.append(pixel_to_draw.rgb)
+            for pixel_to_draw in pixels_to_draw:
+                if pixel_to_draw.rgb not in colors_drawn:
+                    colors_drawn.append(pixel_to_draw.rgb)
 
-			stitch(pixels_to_draw, length)
+            stitch(pixels_to_draw, length)
 
 turtle.exitonclick()
-
